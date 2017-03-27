9ae04ad7210bfaac0b18a7c39dcc70b41e919aa7
Lucas <lucasrolim@poli.ufrj.br>
2017-03-27 20:50:35 -0300

    pipe almost complete

--- pipe/producer-consumer.cpp
+++ pipe/producer-consumer.cpp
@@ -7,10 +7,12 @@
 #include <time.h>
 #include <string>
 #include <sstream>
+
+
 using namespace std;
 
 
-#define stringSize 100
+#define BUFFER 100
 
 
 bool isPrime(int number){
@@ -38,8 +40,8 @@ int main(int argc, char* argv[]) {
     int fd[2]; // File descriptors for Pipe
     pid_t pid; // Store PID number
     int status = pipe(fd); // Flag to errors
-    int inferiorLimit = 0; // Always generate random number bigger than that
-    char readPipe[100];
+    int inferiorLimit = 0; // Always generate random numbers bigger than this limit
+    char str[BUFFER];
 
     // Dealing with erros on pipe
     if(status == -1){
@@ -47,44 +49,43 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    int pidStatus = fork();
     // Dealing with fork error and creating child process
+    int pidStatus = fork();
     if (pidStatus == -1){
         perror("Error creating child process.");
         exit(1);
     }
 
-    //int numberToProduce = atoi(argv[1]);
-    int numberToProduce = 4;
-
+    int numberToProduce = atoi(argv[1]);
+    int wait = 0;
     if(pidStatus > 0){
+    // PRODUCER
         for(int i = 0;i < numberToProduce;i++){
             int product = getRandomNumber(inferiorLimit);
             inferiorLimit = product;
             const char * c = to_string(product).c_str();
             string number = c;
-            cout << "Producer send: " + number << endl;
-            write(fd[1], c, number.size() + 1);
+            cout << "Producer send: " << number << endl;
+            write(fd[1], c, sizeof(str) + 1);
             sleep(1);
         }
-        cout << "All products were sended" << endl;
-        write(fd[1], "0", 2);
+        cout << "All products were sent" << endl;
+        write(fd[1], "0", sizeof("0")+1);
         close(fd[1]);
         exit(0);
     }
     else{
+    // CONSUMER
         while(true){
-            char str_recebida[stringSize];
-            read(fd[0], str_recebida, stringSize);
-            string numericString = str_recebida;
-             cout << str_recebida<< endl;
-            int number = atoi(numericString.c_str());
+            char str_recebida[BUFFER];
+            read(fd[0], str_recebida, sizeof(str_recebida));
+            int number = atoi(str_recebida);
             if (number == 0){
                 break;
             }
-            cout << "Consumer receive: " + numericString << endl;
+            cout << "Consumer received: " << str_recebida << endl;
             bool prime = isPrime(number);
-            cout << "Number " + numericString + " is prime: " << prime << endl;
+            cout << "Number " << str_recebida << " is prime: " << prime << endl;
         }
         cout << "Consumer received 0. Finishing process..." << endl;
         close(fd[0]);
