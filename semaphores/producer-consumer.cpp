8017ad98658afddc39cc7c1ecf8e3ab2a8446f25
Lucas Rolim <lucasrolim@poli.ufrj.br>
2017-04-11 20:39:30 -0300

    refactor, thread and some minor changes. Not include semaphores yet

--- semaphores/producer-consumer.cpp
+++ semaphores/producer-consumer.cpp
@@ -7,7 +7,12 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <thread> 
+#include <mutex> 
+
 using namespace std;
+vector<int> sharedMemory; // vector with N positions (full of zeros)
+vector<int> consumerMemory;
 
 bool isPrime(int number){
     int i;
@@ -19,8 +24,8 @@ bool isPrime(int number){
     return true;
 }
 
-int getRandomNumber(){
-    srand(time(NULL));
+int getRandomNumber(int p){
+    srand(p+time(NULL));
     int number = rand() % 10000000 + 1;
     return number;
 }
@@ -53,44 +58,57 @@ int findProductPosition(vector<int> myVector){
 	return targetPosition;
 }
 
+void producer(int p_id){
+	int positionToStore = findEmptyPosition(sharedMemory); // -1 if the vector is full
+	if(positionToStore != -1){ // MUDAR ESSA CONDIÇÃO PARA SEMÁFORO
+		int product = getRandomNumber(p_id); // p_id is used as part of the seed
+		sharedMemory[positionToStore] = product; // putting the number in the memory
+	}
+}
+
+void consumer(vector<int> sharedMemory,vector<int> &consumerMemory, int &numbersToProcess){
+	int positionToGet = findProductPosition(sharedMemory); // -1 iff the vector is empty
+	if(positionToGet != -1){ // MUDAR ESSA CONDIÇÃO PARA SEMÁFORO
+		cout << sharedMemory[positionToGet]<< " is prime? The answer is: "<< isPrime(sharedMemory[positionToGet])<< endl;
+		consumerMemory.push_back(sharedMemory[positionToGet]); // saving the number in local memory
+		sharedMemory[positionToGet] = 0; // cleaning the position after read a number
+		numbersToProcess -= 1;
+	}
+}
+
+
+
 int main(int argc, char* argv[]) {
-	// first argument is N, the number os spaces in shared memory
-	// second argument is the number of consumer threads
-	// third argument is the number of producer threads
-
-
-	vector<int> sharedMemory (atoi(argv[1]),0); // vector with N positions (full of zeros)
-	vector<int> consumerMemory;
+	int spaces = atoi(argv[1]); // first argument is N, the number os spaces in shared memory
+	int numberConsumerThreads = atoi(argv[2]);  // second argument is the number of consumer threads
+	int numberProducerThreads = atoi(argv[3]);  // third argument is the number of producer threads
+	int numberOfThreads = numberConsumerThreads+numberProducerThreads;
 	int numbersToProcess = 1000; // number of numbers that consumer will process
+	//thread consumerThread[numberConsumerThreads]; // creating consmer threads
+	//thread producerThread[numberProducerThreads]; // creating producer threads
+	thread producerConsumerThreads[numberOfThreads];
+	mutex s_mutex; // mutex semaphore 
+	// FIll memory vector with the argument spaces
+	for (int i = 0; i < spaces; ++i){
+		sharedMemory.push_back(0);
+	}
 
-	/// TEMPORARIAMENTE TESTANTANDO COM APENAS 2 THREADS
-	int pidStatus = fork();
-    if (pidStatus == -1){
-        perror("Error creating child process.");
-        exit(1);
+	// Launch  Threads
+	for(int i = 0; i < numberProducerThreads;i++){
+		producerConsumerThreads[i] = thread(producer,i);
 	}
-	///
-	if(pidStatus > 0){
-		// PRODUCER
-		while (true){
-			int positionToStore = findEmptyPosition(sharedMemory); // -1 if the vector is full
-			if(positionToStore != -1){ // MUDAR ESSA CONDIÇÃO PARA SEMÁFORO
-				int product = getRandomNumber();
-				sharedMemory[positionToStore] = product; // putting the number in the memory
-			}
-		}
+	//for(int i = ProducerThreads; i < numberOfThreads;i++){
+	//	producerConsumerThreads[i] = thread(consumer,sharedMemory,consumerMemory);
+	//}
+	// Join Threads
+	for (int i = 0; i < numberProducerThreads;i++){ // test only producers
+		producerConsumerThreads[i].join();
 	}
-	else{
-		// CONSUMER
-		while(numbersToProcess > 0){
-			int positionToGet = findProductPosition(sharedMemory); // -1 iff the vector is empty
-			if(positionToGet != -1){ // MUDAR ESSA CONDIÇÃO PARA SEMÁFORO
-				cout << sharedMemory[positionToGet]<< " is prime? The answer is: "<< isPrime(sharedMemory[positionToGet])<< endl;
-				consumerMemory.push_back(sharedMemory[positionToGet]); // saving the number in local memory
-				sharedMemory[positionToGet] = 0; // cleaning the position after read a number
-				numbersToProcess -= 1;
-			}
-		}
+
+	// TEST SHARED MEMORY
+
+	for(int i = 0; i < spaces;i++){
+		cout << sharedMemory[i]<< endl;
 	}
 	return 0;
 }
